27a725a3b12ada19afd77f2d962b93da7343ed3a
Pertempto <pertemptodisce@gmail.com>
2025-04-03 23:42:23 -0400

    fix: remove counter

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -55,7 +55,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   final TextEditingController _textController = TextEditingController();
   String _originalText = '';
   String _translatedText = '';
@@ -71,17 +70,6 @@ class _MyHomePageState extends State<MyHomePage> {
     await Future.delayed(const Duration(milliseconds: 500));
     setState(() {
       _translatedText = _originalText.split('').reversed.join();
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
@@ -117,7 +105,8 @@ class _MyHomePageState extends State<MyHomePage> {
               onChanged: (text) {
                 setState(() {
                   _originalText = text;
-                  _translatedText = ''; // Clear translated text when original changes
+                  _translatedText =
+                      ''; // Clear translated text when original changes
                 });
               },
             ),
@@ -132,11 +121,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
